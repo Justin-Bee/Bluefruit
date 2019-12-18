2edfddb309724b15e3e277a4913cd11d349bf256
Justin-Bee <jbee.appz@gmail.com>
2019-12-17 19:27:47 -0500

    updated the uart init function

--- uart.c
+++ uart.c
@@ -6,11 +6,10 @@
  *
  */
 
+/* includes */
  #include "uart.h"
+ #include "nrf52.h"
 
- /* defines */
-  #define ENABLE (*(volatile char *) 0x500)
-  #define BAUDRATE (*(volatile char *) 0x524)
 
  /*
   * uart_init()
@@ -18,13 +17,15 @@
   * This function initializes the uart
   */
  void uart_init(){
+    
+    /* variable for the uart enum referenced in nrf52.h */
+    NRF_UARTE_Type * myUart; 
     /* to enable the UART need to disable other porcesses with same ID - Instantiation Table pg 24. */
 
-
     /* enable the uart by setting 4 to register */
-    ENABLE |= 0x04;
+    myUart->ENABLE |= 0x04;
     /* set the baudrate to 115200 */
-    BAUDRATE |= 0x01D7E000;
+    myUart->BAUDRATE |= 0x01D7E000;
 
 
  }
