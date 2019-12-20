939059a7ba413fcd33d4d425b99c0231aff41351
Justin Bee <jbee2@asu.edu>
2019-12-19 20:16:29 -0500

    Update uart.c

--- uart.c
+++ uart.c
@@ -25,9 +25,9 @@
     /* to enable the UART need to disable other porcesses with same ID - Instantiation Table pg 24. */
     uart->ENABLE = 0;
     /* enable the uart by setting 4 to register */
-    myUart->ENABLE |= 0x04;
+    myUart->ENABLE |= 0x08;
     /* set the baudrate to 115200 */
-    myUart->BAUDRATE |= 0x01D7E000;
+    myUart->BAUDRATE |= 0x01D60000;
 
 
  }
