3f598e2a4b17571312b4810339b02b780c0008ea
Justin-Bee <jbee.appz@gmail.com>
2019-12-25 20:43:59 -0500

    added delay to uart_writestr, now its working

--- uart.c
+++ uart.c
@@ -15,7 +15,7 @@
 
 /* variable for the uart enum referenced in nrf52.h */
 //NRF_UARTE_Type * myUart = 0x40002000; 
-NRF_UART_Type * uart;// = 0x40002000;
+NRF_UART_Type * uart = 0x40002000;
 
 
 
@@ -31,25 +31,24 @@ NRF_UART_Type * uart;// = 0x40002000;
   * This function initializes the uart
   */
  void uart_init(){
+    /* set the pinselect to the uart pin */
+    uart->PSELTXD = UARTTX;
+    uart->PSELRXD = UARTRX;
+    uart->PSELCTS = UARTCTS;
+    uart->PSELRTS = UARTRTS;
 
     /* to enable the UART need to disable other porcesses with same ID - Instantiation Table pg 24. */
    // uart->ENABLE = 0;
     /* enable the UART by setting 4 to enable*/
     uart->ENABLE = 4;
-
     uart->SHORTS = 0;
     uart->INTENSET = 0;
     uart->INTENCLR = 0;
-    /* set the pinselect to the uart pin */
-  //  uart->PSELTXD = UARTTX;
-  //  uart->PSELRXD = UARTRX;
-  //  uart->PSELCTS = UARTCTS;
- //   uart->PSELRTS = UARTRTS;
     /* config the parity */
    // uart->CONFIG = 0x07;
     /* set the baudrate to 115200 */
-    //uart->BAUDRATE = 0x01D7E000UL;
-    uart->BAUDRATE = 0x00275000UL; //9600 baud 
+    uart->BAUDRATE = 0x01D7E000UL;
+ 
  }
 
 /*
@@ -58,13 +57,10 @@ NRF_UART_Type * uart;// = 0x40002000;
  * This function prints a str to uart 
  */
 void uart_writechar(char str){
-    //while(!uart->EVENTS_CTS){}
+    /* start the transmission sequence */
     uart->TASKS_STARTTX=1;
+    /* write the character to the TDX */
     uart->TXD = str; 
-    while(!uart->EVENTS_TXDRDY){}
-   // while(!uart->EVENTS_CTS){}
-   uart->TASKS_STARTTX;
-
 }
 
 /*
@@ -80,11 +76,20 @@ void uart_writechar(char str){
     int length= strlen(str);
   
     /* iterate through the chars */
-    for(int i=0; i<length; i++){
+    for(int i=0; i<=length; i++){
+      /* write the current character */
       uart_writechar(str[i]); 
+      /* wait for the rdy signal */
+      while(!uart->EVENTS_TXDRDY){}
+      /* added delay for uart(wouldn't work without it */
+      unsigned long tmo = 1000000;
+      while(tmo >0){
+      tmo--;
+      }
+      /* stop the transmission */
+      uart->TASKS_STOPTX;
     }
-    /* stop the transmission */
-   // uart->TASKS_STOPTX;
+
  }
 
 
