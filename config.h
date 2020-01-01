18348363e0d6cff34d716f5bfae59315342b53d2
Justin-Bee <jbee.appz@gmail.com>
2020-01-01 18:26:49 -0500

    added to config struct

---
+++ config.h
@@ -0,0 +1,27 @@
+/*
+ * config.h
+ *
+ * @Author: Justin Bee
+ * @Date: 12/31/2019
+ *
+ * This file contains the function declarations for
+ * the config.c class
+ */
+
+ #ifndef _CONFIG_H
+ #define _CONFIG_H
+
+/* the config struct */
+typedef struct{
+  char[5] name;
+    
+
+}config_struct;
+
+/* initialize the struct */
+void config_init();
+
+
+
+
+ #endif //_CONFIG_H
