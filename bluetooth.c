989b5b97bb298637aa359c94183d3ccae43a350d
Justin-Bee <jbee.appz@gmail.com>
2020-01-26 13:05:33 -0500

    Added files for the bluetooth functions

---
+++ bluetooth.c
@@ -0,0 +1,34 @@
+/*
+ * bluetooth.c
+ * 
+ * @Author: Justin Bee
+ * @Date: 1/26/2020
+ *
+ * This file contains all of the bluetooth functions
+ */
+
+/* includes */
+#include "bluetooth.h"
+
+
+/*
+ * bluetooth_init()
+ *
+ * Intitialize the bluetooth
+ */
+void bluetooth_init(){
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+/*************************** End of file ****************************/
---
+++ bluetooth.h
@@ -0,0 +1,30 @@
+/*
+ * bluetooth.h
+ *
+ * @Author: Justin Bee
+ * @Date: 1/26/2020
+ *
+ * This file contains the function declarations for
+ * the bluetooth.c class
+ */
+
+
+#ifndef _BLUETOOTH_H_INCLUDED
+#define _BLUETOOTH_H_INCLUDED
+
+/* bluetooth_init() */
+void bluetooth_init();
+
+
+
+
+
+
+
+
+
+
+#endif /* _BLUETOOTH_H_INCLUDED
+
+
+/*************************** End of file ****************************/
